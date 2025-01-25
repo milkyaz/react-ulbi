d1f47d62f1fadeadd2e4719bdaada0ff83e21892
Daniil Zaburunov <zaburunov.2000@mail.ru>
2025-01-25 23:02:25 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState, useRef } from "react";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import "./styles/App.css";
+
+function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "JavaScript", body: "Description" },
+    { id: 2, title: "JavaScript 2", body: "Description" },
+    { id: 3, title: "JavaScript 3", body: "Description" },
+  ]);
+  const [post, setPost] = useState({ title: "", body: "" });
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    setPosts([...posts, { ...post, id: Date.now() }]);
+    setPost({ title: "", body: "" });
+  };
+  return (
+    <div className="App">
+      <form>
+        <MyInput
+          value={post.title}
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
+          type="text"
+          placeholder="Название поста"
+        />
+        <MyInput
+          value={post.body}
+          onChange={(e) => setPost({ ...post, body: e.target.value })}
+          type="text"
+          placeholder="Описание поста"
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов" />
+    </div>
+  );
+}
+
+export default App;
